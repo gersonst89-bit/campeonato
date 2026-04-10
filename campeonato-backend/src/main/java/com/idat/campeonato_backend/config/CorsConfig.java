fab09e32606499876c24df9cd1cf615a1ea6498c
Gerson <gersonst89@gmail.com>
2026-04-09 23:56:21 -0500

    Add CORS config

---
+++ campeonato-backend/src/main/java/com/idat/campeonato_backend/config/CorsConfig.java
@@ -0,0 +1,27 @@
+package com.idat.campeonato_backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(
+                                "http://localhost:4200",
+                                "https://campeonato-theta.vercel.app"
+                        )
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+}
+
